45662d6bad6b06385844c60b9277985addf3ff6d
Alyssa Milburn <fuzzie@fuzzie.org>
2012-02-10 22:41:09 +0100

    AGS: Read game script.

--- engines/ags/gamefile.cpp
+++ engines/ags/gamefile.cpp
@@ -27,6 +27,7 @@
 #include "ags/constants.h"
 #include "ags/gamefile.h"
 #include "ags/resourceman.h"
+#include "ags/script.h"
 
 namespace AGS {
 
@@ -262,9 +263,11 @@ bool GameFile::init(const ResourceManager &resMan) {
 			_dict._words[i]._word = decryptString(dta);
 			_dict._words[i]._id = dta->readUint16LE();
 		}
-		// FIXME: dict
-		error("don't support dict yet");
 	}
+
+	// FIXME
+	ccScript gameScript;
+	gameScript.readFrom(dta);
 
 	delete dta;
 
