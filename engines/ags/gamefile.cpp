d5cc3a4e7ecda016c55f153284c57e3b14145d4d
Alyssa Milburn <fuzzie@fuzzie.org>
2012-02-13 08:50:05 +0100

    AGS: Fill in some more startup and scripting bits.

--- engines/ags/gamefile.cpp
+++ engines/ags/gamefile.cpp
@@ -102,7 +102,7 @@ bool GameFile::init() {
 
 	_viewCount = dta->readUint32LE();
 	_charCount = dta->readUint32LE();
-	_playerChars = dta->readUint32LE();
+	_playerChar = dta->readUint32LE();
 
 	_totalScore = dta->readSint32LE();
 
@@ -114,9 +114,9 @@ bool GameFile::init() {
 	_fontCount = dta->readUint32LE();
 
 	debug(2,
-	      "%d views, %d characters, %d player characters, %d inventory items, "
-	      "%d dialog topics",
-	      _viewCount, _charCount, _playerChars, _invItemCount, _dialogCount);
+	      "%d views, %d characters (player is %d), %d inventory items, %d "
+	      "dialog topics",
+	      _viewCount, _charCount, _playerChar, _invItemCount, _dialogCount);
 
 	_colorDepth = dta->readUint32LE();
 
@@ -446,7 +446,7 @@ NewInteraction *GameFile::readNewInteraction(Common::SeekableReadStream *dta) {
 			error("invalid interaction? (%d)", unknown);
 		return NULL;
 	}
-	debug(1, "new interaction");
+	debug(8, "new interaction");
 
 	NewInteraction *interaction = new NewInteraction();
 
@@ -468,7 +468,7 @@ NewInteraction *GameFile::readNewInteraction(Common::SeekableReadStream *dta) {
 		if (!hasResponse[i])
 			continue;
 
-		debug(1, "reading response");
+		debug(8, "reading response");
 		interaction->_events[i]._response = readCommandList(dta);
 	}
 
