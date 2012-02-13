4efc3e5929d111c0b5a9ab71e4a4db10afc0b3dd
Alyssa Milburn <fuzzie@fuzzie.org>
2012-02-13 21:42:17 +0100

    AGS: More script engine work.

--- engines/ags/script.h
+++ engines/ags/script.h
@@ -64,7 +64,7 @@ struct ccScript {
 };
 
 enum RuntimeValueType {
-	rvtInvalid,
+	rvtInvalid = 0,
 	rvtInteger,
 	rvtGlobalData,
 	rvtFunction,
@@ -123,7 +123,8 @@ protected:
 	ccInstance *_runningInst;
 
 	void pushValue(const RuntimeValue &value);
-	uint32 popValue();
+	RuntimeValue popValue();
+	uint32 popIntValue();
 };
 
 } // End of namespace AGS
