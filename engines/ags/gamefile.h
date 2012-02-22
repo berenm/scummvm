4cff7cc9b658ce98199e3f74d7795420d8bb9b31
Alyssa Milburn <fuzzie@fuzzie.org>
2012-02-22 20:01:25 +0100

    AGS: Move characters array to engine.

--- engines/ags/gamefile.h
+++ engines/ags/gamefile.h
@@ -231,8 +231,6 @@ public:
 
 	Common::Array<Common::String> _messages;
 
-	Common::Array<Character *> _chars;
-
 	Common::Array<DialogTopic> _dialogs;
 	Common::Array<Common::String> _speechLines;
 
